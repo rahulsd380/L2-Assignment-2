1e50d3dd886cacbfcbcdbf42f7e1b22c24b9798f
rahulsd380 <rahulsd380@gmail.com>
2024-05-22 21:37:38 +0600

    reduce quantity implemented

--- src/modules/orders/order.controller.ts
+++ src/modules/orders/order.controller.ts
@@ -1,15 +1,43 @@
 import { Request, Response } from "express";
 import { orderServices } from "./order.service";
 import OrdersZodSchema from "./order.validation";
+import { productServices } from "../products/products.service";
 
 // Create new order
 const createNewOrder = async (req: Request, res: Response) => {
   try {
-    
     const orderData = req.body;
     const zodValidatedData = OrdersZodSchema.parse(orderData);
 
+    // Find the product by productId
+    const product = await productServices.getSpecificProduct(zodValidatedData.productId);
+
+    if (!product) {
+      return res.status(404).json({
+        success: false,
+        message: "Product not found",
+      });
+    }
+
+    // Checking the quantity here
+    if (product.inventory.quantity < zodValidatedData.quantity) {
+      return res.status(400).json({
+        success: false,
+        message: "Insufficient product quantity",
+      });
+    }
+
     const result = await orderServices.createNewOrder(zodValidatedData);
+
+    // Reduce the product quantity in the products collection
+    product.inventory.quantity -= zodValidatedData.quantity;
+
+    // Check if quantity is zero and update inStock field
+    if (product.inventory.quantity === 0) {
+      product.inventory.inStock = false;
+    }
+
+    await product.save();
 
     res.json({
       success: true,
@@ -19,7 +47,7 @@ const createNewOrder = async (req: Request, res: Response) => {
   } catch (error: any) {
     res.status(400).json({
       success: false,
-      message: "Couldn't create product. Please try again!",
+      message: "Couldn't create order. Please try again!",
     });
   }
 };
